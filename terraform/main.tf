772a37ceb5b1c0e5a0d29655c2815dc58ef9980d
github-carlos <caeduob@gmail.com>
2023-07-09 17:00:52 -0300

    pipe terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.45.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
+  }
+  backend "s3" {
+    bucket = "asksuite-bot-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
   }
 }
