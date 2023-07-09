713e087bbbc22fe437f2a275f6a6a58baa108d38
github-carlos <caeduob@gmail.com>
2023-07-09 19:22:35 -0300

    add health check on target group

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,7 +1,7 @@
 resource "aws_alb" "application_load_balancer" {
   name               = "load-balancer-dev"
   load_balancer_type = "application"
-  subnets = [ 
+  subnets = [
     "${aws_default_subnet.default_subnet_a.id}",
     "${aws_default_subnet.default_subnet_b.id}"
   ]
@@ -30,15 +30,24 @@ resource "aws_lb_target_group" "target_group" {
   port        = 8080
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = "${aws_default_vpc.default_vpc.id}" # default VPC
+  vpc_id      = aws_default_vpc.default_vpc.id # default VPC
+  health_check {
+    path                = "/health"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 20
+    interval            = 30
+  }
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" 
+  load_balancer_arn = aws_alb.application_load_balancer.arn
   port              = 80
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+    target_group_arn = aws_lb_target_group.target_group.arn
   }
 }
