1d8c60cc80f8906a64dc05bc16150e89abaa1679
github-carlos <caeduob@gmail.com>
2023-07-09 21:25:40 -0300

    changes memory of container

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -3,14 +3,14 @@ resource "aws_ecs_cluster" "my_cluster" {
 }
 
 data "aws_ecr_image" "ecr_image" {
-  repository_name = aws_ecr_repository.repo-crawler.name
-  image_tag       = "latest"
+  repository_name = "${aws_ecr_repository.repo-crawler.name}"
+  image_tag = "latest"
 }
 
 resource "aws_ecs_task_definition" "app_task" {
   family                   = "app-first-task" # Name your task
-  cpu                      = 1024
-  memory                   = 2048
+  cpu = 512
+  memory = 2048
   container_definitions    = <<DEFINITION
   [
     {
@@ -23,19 +23,19 @@ resource "aws_ecs_task_definition" "app_task" {
           "hostPort": 8080
         }
       ],
-      "memory": 1024,
+      "memory": 2048,
       "cpu": 512
     }
   ]
   DEFINITION
   requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
   network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
-  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "ecsTaskExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -50,26 +50,26 @@ data "aws_iam_policy_document" "assume_role_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = aws_iam_role.ecsTaskExecutionRole.name
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_ecs_service" "app_service" {
-  name            = "app-first-service"
-  cluster         = aws_ecs_cluster.my_cluster.id
-  task_definition = aws_ecs_task_definition.app_task.arn
+  name            = "app-first-service"     
+  cluster         = "${aws_ecs_cluster.my_cluster.id}" 
+  task_definition = "${aws_ecs_task_definition.app_task.arn}"
   launch_type     = "FARGATE"
   desired_count   = 1
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.target_group.arn # Reference the target group
-    container_name   = aws_ecs_task_definition.app_task.family
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Reference the target group
+    container_name   = "${aws_ecs_task_definition.app_task.family}"
     container_port   = 8080
   }
 
   network_configuration {
     subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
-    assign_public_ip = true                                                # Provide the containers with public IPs
+    assign_public_ip = true     # Provide the containers with public IPs
     security_groups  = ["${aws_security_group.service_security_group.id}"] # Set up the security group
   }
 }
